69344d02f54e7b3980b49e1381fae267ef38be13
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2024-01-10 19:41:32 -0500

    User can set Department & Password

--- src/Email.java
+++ src/Email.java
@@ -11,49 +11,76 @@ public class Email {
     private  String department;
 
     private String email;
+    private int passwordLength = 8;
     private String password;
     private String alternateEmail;
-
     private int mailboxCapacity;
+
+    private String companyEmail = "jacksonInc.com";
+
+    Scanner scan = new Scanner(System.in);
     public Email(String firstName,String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
-
+        System.out.println("EMAIL CREATED: " + firstName + " " + lastName);
         setDepartment();
+        System.out.println();
+        System.out.println("Department: " + this.department);
+        System.out.println();
+        this.password = generatePassword(passwordLength);
+        System.out.println("Your newly generated password is: " + this.password);
+
+        System.out.println();
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + this.department + "." + companyEmail;
+
+        System.out.println("Your Email: " + email);
     }
 
     private void setDepartment(){
-        System.out.print("Enter Department: \n"
+        System.out.print("Department Numbers: \n"
                 + "1. Sales \n"
                 + "2. Development\n"
-                + "3. Accounting \n"
-                + "0. Accounting \n");
-        Scanner scan = new Scanner(System.in);
+                + "3. Accounting \n\n");
+
+        System.out.print("Enter Department Number: ");
+
         int choice = scan.nextInt();
+
 
         switch(choice){
             case 1:
-                this.department = "Sales";
+                this.department = "sales";
                 break;
             case 2:
-                this.department = "Development";
+                this.department = "development";
                 break;
             case 3:
-                this.department = "Accounting";
+                this.department = "accounting";
+                break;
+            case 4:
+                this.department = "hr";
+                break;
+            case 5:
+                this.department = "default";
                 break;
             default:
-                this.department = "";
                 break;
+
         }
-
-        return;
     }
 
 
-    public void generatePassword(){
-        Random rand = new Random();
-        this.password = firstName;
+
+    private String generatePassword(int length){
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz1234567890!@#$%^&*()?{}|<>";
+        char[] password = new char[length];
+        for(int i = 0; i < length; i++){
+           int rand = (int) (Math.random() * passwordSet.length());
+           password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
     }
+    
     public void setPassword(String password){
         this.password = password;
     }
@@ -62,14 +89,6 @@ public class Email {
         this.password = firstName;
     }
 
-
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
-    }
-
-    public String getEmail(){
-        return email;
-    }
 
     public void fullName(){
         System.out.println(firstName + " " + lastName);
--- src/EmailApp.java
+++ src/EmailApp.java
@@ -2,5 +2,7 @@ public class EmailApp {
     public static void main(String[] args) {
 
         Email email = new Email("John","Morgan");
+
+
     }
 }
